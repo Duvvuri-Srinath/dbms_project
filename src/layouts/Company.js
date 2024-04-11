55813097b00e1f6656300ab89f9658301932f5f6
duvvurisrinath <duvvurisrinath@gmail.com>
2024-04-12 04:02:37 +0530

    fixed bug

--- src/layouts/Company.js
+++ src/layouts/Company.js
@@ -58,7 +58,7 @@ function Company() {
   useEffect(() => {
     const is_valid = async () => {
       try {
-        const response = await fetch("http://localhost:3000/s", {
+        const response = await fetch("http://localhost:3000/c", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
