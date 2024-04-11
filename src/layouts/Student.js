6a47fce26e812bf9f3a01109fb972c03d9c093b3
g-nitin-1 <21cs01007@iitbbs.ac.in>
2024-04-11 21:14:42 +0530

    temp auth

--- src/layouts/Student.js
+++ src/layouts/Student.js
@@ -14,8 +14,37 @@ function Student() {
   const [image, setImage] = React.useState(sidebarImage);
   const [color, setColor] = React.useState("black");
   const [hasImage, setHasImage] = React.useState(true);
+  const [status, setStatus] = React.useState("valid");
+  const [role, setRole] = React.useState("student");
   const location = useLocation();
   const mainPanel = React.useRef(null);
+  const is_valid = async () => {
+    try {
+      console.log("token : ", localStorage.getItem("token"));
+      const response = await fetch("http://localhost:3000/s", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      })
+        .then((response) => {
+          console.log(response, "here");
+          return response;
+        })
+        .catch((err) => console.log("Fetch Error: ", err));
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const jsonData = await response.json();
+      console.log("jsondatda:", jsonData);
+      setStatus(jsonData.status); // Accessing the 'rows' array in the response
+      setRole(jsonData.role); // Accessing the 'columns' array in
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const getRoutes = (routes) => {
     return routes.map((prop, key) => {
       if (prop.layout === "/s") {
@@ -33,11 +62,11 @@ function Student() {
   };
   const needRoutes = routes.filter((item) => {
     return item.layout === "/s";
-  })
+  });
   React.useEffect(() => {
-    document.documentElement.scrollTop = 0;
-    document.scrollingElement.scrollTop = 0;
-    mainPanel.current.scrollTop = 0;
+    // document.documentElement.scrollTop = 0;
+    // document.scrollingElement.scrollTop = 0;
+    // mainPanel.current.scrollTop = 0;
     if (
       window.innerWidth < 993 &&
       document.documentElement.className.indexOf("nav-open") !== -1
@@ -47,20 +76,29 @@ function Student() {
       element.parentNode.removeChild(element);
     }
   }, [location]);
+  is_valid();
   return (
     <>
-      <div className="wrapper">
-        <Sidebar color={color} image={hasImage ? image : ""} routes={needRoutes} />
-        <div className="main-panel" ref={mainPanel}>
-          <StudentNavbar />
-          {/* {<TableList/>} */}
-          <div className="content">
-            <Switch>{getRoutes(routes)}</Switch>
+      {status === "valid" && role === "student" ? (
+        <div className="wrapper">
+          <Sidebar
+            color={color}
+            image={hasImage ? image : ""}
+            routes={needRoutes}
+          />
+          <div className="main-panel" ref={mainPanel}>
+            <StudentNavbar />
+            {/* {<TableList/>} */}
+            <div className="content">
+              <Switch>{getRoutes(routes)}</Switch>
+            </div>
           </div>
-          
         </div>
-      </div>
-     
+      ) : (
+        <div>
+          <h1>Unauthorized</h1>
+        </div>
+      )}
     </>
   );
 }
